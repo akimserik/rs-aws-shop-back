8b20a986805141e996e718555bb3bb0d9b7392ba
akimserik <s.akimgereyev@gmail.com>
2024-06-12 13:49:30 +0500

    enable cors

--- lambda/getProductsList.ts
+++ lambda/getProductsList.ts
@@ -3,7 +3,11 @@ import products from "./mockData";
 exports.handler = async (event: any) => {
   return {
     statusCode: 200,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(products),
   };
 };
