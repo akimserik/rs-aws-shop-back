5801f273a84d442fd33dbc1b88ed6e8045236d87
akimserik <s.akimgereyev@gmail.com>
2024-06-19 13:55:21 +0500

    add scripts to populate data

--- lambda/mockProducts.ts
+++ lambda/mockProducts.ts
@@ -1,23 +1,24 @@
-import { Product } from "../types/product";
-
-const products: Product[] = [
+const products = [
   {
     id: "1",
     title: "Acoustic Guitar",
     description: "A high-quality acoustic guitar with a warm, vibrant sound.",
     price: 300,
+    stock: 3,
   },
   {
     id: "2",
     title: "Electric Piano",
     description: "An 88-key electric piano with a variety of sound settings.",
     price: 750,
+    stock: 2,
   },
   {
     id: "3",
     title: "Violin",
     description: "A beautifully crafted violin with a rich, melodious tone.",
     price: 450,
+    stock: 4,
   },
   {
     id: "4",
@@ -25,12 +26,14 @@ const products: Product[] = [
     description:
       "A 5-piece drum set with cymbals, perfect for any style of music.",
     price: 700,
+    stock: 1,
   },
   {
     id: "5",
     title: "Flute",
     description: "A silver-plated concert flute for players of all levels.",
     price: 200,
+    stock: 5,
   },
 ];
 
